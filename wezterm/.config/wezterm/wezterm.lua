822d121023ba39802c2bd464428de0eb422e52c2
keypeearr <kyle.requez.code@gmail.com>
2026-05-11 13:21:45 +0800

    Updated wezterm config
    - Changed font size from 14 px to 16 px

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -10,7 +10,7 @@ config.front_end = "WebGpu"
 
 config.color_scheme = "Black Metal (Burzum) (base16)"
 config.font = wezterm.font("JetBrains Mono")
-config.font_size = 14
+config.font_size = 16
 
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 20
